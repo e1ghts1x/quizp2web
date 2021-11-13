b06f28aef826cda1d672045f4b4d5dba0791ea8a
BuildTools <unconfigured@null.spigotmc.org>
2021-11-13 03:41:01 -0300

    fixed alot lul :>

--- QUIZ/scripts/validar.js
+++ QUIZ/scripts/validar.js
@@ -1,53 +1,51 @@
-const name = document.getElementById("name");
+const nome = document.getElementById("name");
 const email = document.getElementById("email");
 const cpf = document.getElementById("cpf");
 const elementName = document.getElementById("elementName");
 const elementEmail = document.getElementById("elementEmail");
 const elementCpf = document.getElementById("elementCpf");
+const botao = document.querySelector("button");
 
 
 // Validar Nome
 function validarNome(){
-    if(name.value.trim()===""){ //consertar
-        name.style.background = "#5952d4";
+    if(nome.value.trim()===""){
+        nome.style.background = "#5952d4";
         elementName.innerHTML = "Insira um Username válido";
-        return false;
+        botao.disabled = true;
     }
     else{
-        name.style.background = "#161923";
+        nome.style.background = "#161923";
         elementName.innerHTML = "Username"
-        return true;
+        botao.disabled = false;
     }
 }
-
-//Verificar e impedir a existência de números ou símbolos... 
 
 // Validar Email
 function validarEmail(){
     if(email.value.trim()==="" || !email.value.includes("@") || !email.value.includes(".com")){
         email.style.background = "#5952d4";
         elementEmail.innerHTML = "Insira um Email válido";
-        return false;
+        botao.disabled = true;
     }
     else{
         email.style.background = "#161923";
         elementEmail.innerHTML = "E-mail";
-        return true;
+        botao.disabled = false;
     }
 }
 
-
 //Validar CPF
 function validarCPF(){
-    if(cpf.value.trim()==="" || cpf.value.trim() < 12){
+    if(cpf.value.trim()==="" || cpf.value.length < 14){
         cpf.style.background = "#5952d4";
         elementCpf.innerHTML = "Insira um CPF válido"
-        return false;
+        botao.disabled = true;
     }
     else{
-        cpf.style.background == "#161923";
+        cpf.style.background = "#161923";
         elementCpf.innerHTML = "CPF";
-        return false;
+        botao.disabled = false;
     }
 }
 
@@ -55,5 +53,10 @@ function onlyNumber(){
     cpf.value = cpf.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
 }
 
+function onlyLetters(){
+    nome.value = nome.value.replace(/[0-9.]/g, '');
+}
 
-//finalizar
+$(document).ready(function(){
+    $("#cpf").mask("999.999.999-99");
+});
