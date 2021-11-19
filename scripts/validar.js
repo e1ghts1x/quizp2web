105711b5b57ba7e8d473d3fa686f35312eb4df43
e1ghts1x <ekgamesbr@outlook.com>
2021-11-19 20:33:01 -0300

    Maybe Last Update

--- scripts/validar.js
+++ scripts/validar.js
@@ -6,7 +6,6 @@ const elementEmail = document.getElementById("elementEmail");
 const elementCpf = document.getElementById("elementCpf");
 const botao = document.querySelector("button");
 
-
 // Validar Nome
 function validarNome(){
     if(nome.value.trim()===""){
@@ -36,9 +35,35 @@ function validarEmail(){
 }
 
 //Validar CPF
-function validarCPF(){
-    soma = (cpf.value)
-    if(cpf.value.trim()==="" || cpf.value.length < 14){
+
+function TestaCPF() {
+    var Soma;
+    var Resto;
+    Soma = 0;
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(cpf.value.trim().substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.value.trim().substring(9, 10)) ) botao.disabled = true;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.value.trim().substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.value.trim().substring(10, 11) ) ) botao.disabled = true; 
+
+    if(cpf.value.trim()==="" || cpf.value.length != 11 || cpf.value.trim()==="00000000000" || 
+    cpf.value.trim()==="11111111111" || 
+    cpf.value.trim()==="22222222222" || 
+    cpf.value.trim()==="33333333333" ||
+    cpf.value.trim()==="44444444444" ||
+    cpf.value.trim()==="55555555555" ||
+    cpf.value.trim()==="66666666666" ||
+    cpf.value.trim()==="77777777777" ||
+    cpf.value.trim()==="88888888888" ||
+    cpf.value.trim()==="99999999999") {
         cpf.style.background = "#5952d4";
         elementCpf.innerHTML = "Insira um CPF válido"
         botao.disabled = true;
@@ -62,6 +87,6 @@ function onlyLetters(){
 
 
 //Máscara de CPF em jquery
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#cpf").mask("999.999.999-99");
-});
+});*/
